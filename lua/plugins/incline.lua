10239c3e8c162022bab0cb96d50f93ba9a487eee
TheCedarPrince <jacobszelko@gmail.com>
2023-04-16 23:51:54 -0400

    Modified incline to work properly

--- lua/plugins/incline.lua
+++ lua/plugins/incline.lua
@@ -2,13 +2,14 @@ return {
 	"b0o/incline.nvim",
 	lazy = false,
 	config = function()
+	require("incline").setup({
 	debounce_threshold = {
 		falling = 50,
 		rising = 10
 	},
 	hide = {
 		cursorline = false,
-		focused_win = false,
+		focused_win = true,
 		only_win = true -- Hide incline if only one window in tab
 	},
 	highlight = {
@@ -30,38 +31,16 @@ return {
 		unlisted_buffers = true,
 		wintypes = "special"
 	},
-	render = function(props)
-		local fname = vim.fn.fnamemodify(vim.api.nvim_buf_get_name(props.buf), ':t')
-		local colors = require('gruvbox.palette')
-
-		if props.focused == true then
-			return {
-				{
-					fname,
-					guibg = colors.dark0_hard,
-					guifg = colors.light1
-				}
-			}
-		else
-			return {
-				{
-					fname,
-					guibg = colors.dark0_hard,
-					guifg = colors.dark4
-				}
-			}
-		end
-	end,
 	window = {
 		margin = {
 			horizontal = 1,
-			vertical = 2
+			vertical = 1
 		},
 		options = {
 			signcolumn = "no",
-			wrap = false
+			wrap = true
 		},
-		padding = 0,
+		padding = 1,
 		padding_char = " ",
 		placement = {
 			horizontal = "right",
@@ -82,5 +61,6 @@ return {
 		},
 		zindex = 50
 	}
+})
 end
 }
