7216b0a20e1f4a2cf2faac2fe815eb0b1d46f656
TheCedarPrince <jacobszelko@gmail.com>
2023-05-20 22:13:11 -0400

    Configured previews

--- lua/plugins/markdown-preview.lua
+++ lua/plugins/markdown-preview.lua
@@ -8,5 +8,8 @@ vim.g.mkdp_refresh_slow = 0 -- Refresh preview while editing
 vim.g.mkdp_page_title = '${name}' -- Preview page title
 
 vim.g.mkdp_theme = 'light' -- Set default theme (dark or light)
+vim.g.mkdp_echo_preview_url = 1
+vim.g.mkdp_port = '8080'
+vim.g.mkdp_page_title = '「 Preview 」'
 end
 }
