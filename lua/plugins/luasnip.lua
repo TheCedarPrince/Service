c583665faa5f67741ec3fcc63c37061d5cf182f9
TheCedarPrince <jacobszelko@gmail.com>
2023-07-07 14:30:00 -0400

    Added support for Suffice

--- lua/plugins/luasnip.lua
+++ lua/plugins/luasnip.lua
@@ -4,5 +4,7 @@ return {
 	config = function()
 		require("luasnip/loaders/from_vscode").lazy_load()
 		require("luasnip/loaders/from_snipmate").lazy_load({ paths = {"~/.config/nvim/snippets"} })
+    -- Added this line to support the Suffice configuration
+		require("luasnip/loaders/from_snipmate").lazy_load({ paths = {"~/.config/nvim/lua/core/snippets"} })
 	end
 }
