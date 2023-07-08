c1eac5178001d824f82357a57a452146324812cb
TheCedarPrince <jacobszelko@gmail.com>
2023-07-07 20:08:04 -0400

    Disabled screen dimming

--- lua/plugins/hop.lua
+++ lua/plugins/hop.lua
@@ -8,6 +8,10 @@ return {
 	case_insensitive = true,
 	multi_windows = true
 }
+
+-- hop
+require'hop'.setup()
+
 -- HopLineStart
 vim.api.nvim_set_keymap('n', '<C-h>l', "<cmd>lua require'hop'.hint_lines_skip_whitespace()<cr>", {})
 
@@ -19,6 +23,11 @@ vim.api.nvim_set_keymap('n', '<C-h>c', "<cmd>lua require'hop'.hint_char1()<cr>",
 
 -- HopPattern
 vim.api.nvim_set_keymap('n', '<C-h>p', "<cmd>lua require'hop'.hint_patterns()<cr>", {})
+
+vim.api.nvim_command('highlight HopNextKey guifg=none guibg=none gui=none ctermfg=none cterm=bold')
+vim.api.nvim_command('highlight HopNextKey1 guifg=none guibg=none gui=none ctermfg=none cterm=bold')
+vim.api.nvim_command('highlight HopNextKey2 guifg=none guibg=none gui=none ctermfg=none')
+vim.api.nvim_command('highlight HopUnmatched guifg=none guibg=none guisp=none ctermfg=none')
 
     end
   }
