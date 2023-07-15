119c2b1e4974ff3b60c09f3077082d0d290e41e7
TheCedarPrince <jacobszelko@gmail.com>
2023-07-15 18:16:32 -0400

    Added additional filetypes to allow markdown preview to work

--- lua/plugins/markdown-preview.lua
+++ lua/plugins/markdown-preview.lua
@@ -11,5 +11,6 @@ vim.g.mkdp_theme = 'light' -- Set default theme (dark or light)
 vim.g.mkdp_echo_preview_url = 1
 vim.g.mkdp_port = '8080'
 vim.g.mkdp_page_title = '「 Preview 」'
+vim.g.mkdp_filetypes = { "markdown", "pandoc", "anki", "text", "txt" } 
 end
 }
