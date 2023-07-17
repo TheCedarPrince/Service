eb5bd0d60f845e44b128030717aac992e4efc0a4
TheCedarPrince <jacobszelko@gmail.com>
2023-07-16 23:39:33 -0400

    Updated NewNote function thanks to @max397574

--- lua/functions.lua
+++ lua/functions.lua
@@ -1,28 +1,23 @@
-function NewNote(...)
-  local args = {...}
-  local luasnip = require("luasnip")
+local function new_note(args)
 
-  -- Get the current UTC time
-  local timestamp = os.date("!%m%d%Y%H%M%S")
+    local luasnip = require("luasnip")
+    -- Get the current UTC time
+    local timestamp = os.date("!%m%d%Y%H%M%S")
 
-  -- Create the filename based on the timestamp and arguments
-  local filename = timestamp .. "-" .. table.concat(args, "-") .. ".md"
+    vim.cmd.edit(timestamp .. "-" .. table.concat(args.fargs, "-") .. ".md")
 
-  -- Create the new file
-  local file = io.open(filename, "w")
-  file:close()
-
-  -- Open the file in a new buffer
-  vim.cmd("edit " .. filename)
-
-  -- Get "zettel" snippet and expand it in current buffer
-  for _, snippet in pairs(luasnip.get_snippets("pandoc")) do
-			if snippet.name == "zettel" then
-				luasnip.snip_expand(snippet, {})
-				return
-			end
-		end
+    -- Get "zettel" snippet and expand it in current buffer
+    for _, snippet in pairs(luasnip.get_snippets("pandoc")) do
+        if snippet.name == "zettel" then
+            luasnip.snip_expand(snippet, {})
+            return
+        end
+    end
 end
 
--- Creates a new note within the current directory
-vim.cmd("command! -nargs=* NewNote lua NewNote(<f-args>)")
+vim.api.nvim_create_user_command("NewNote", function(...)
+    new_note(...)
+end, {
+    nargs = "*",
+})
+
