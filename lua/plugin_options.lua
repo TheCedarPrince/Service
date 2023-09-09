3bde5146b64c06799170e57d4bad5be6429659cc
TheCedarPrince <jacobszelko@gmail.com>
2023-09-09 13:00:14 -0400

    Cleared default colors for WhichKey and Pmenu

--- lua/plugin_options.lua
+++ lua/plugin_options.lua
@@ -2,3 +2,12 @@
 vim.cmd("hi GitSignsAdd guibg=NONE ctermfg=white")
 vim.cmd("hi GitSignsChange guibg=NONE ctermfg=white")
 vim.cmd("hi GitSignsDelete guibg=NONE ctermfg=white")
+
+vim.api.nvim_set_hl(0, "WhichKey", { fg = "None" })
+vim.api.nvim_set_hl(0, "WhichKeyBorder", { fg = "None" })
+vim.api.nvim_set_hl(0, "WhichKeyDesc", { fg = "None" })
+vim.api.nvim_set_hl(0, "WhichKeyFloat", { fg = "None" })
+vim.api.nvim_set_hl(0, "WhichKeyGroup", { fg = "None" })
+vim.api.nvim_set_hl(0, "WhichKeySeparator", { fg = "None" })
+vim.api.nvim_set_hl(0, "WhichKeyValue", { fg = "None" })
+vim.api.nvim_set_hl(0, "Pmenu", { ctermbg = "None" })
