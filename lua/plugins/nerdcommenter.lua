7fd7a24fdb7023f1b0c8c6a2d220c38ef68a0168
TheCedarPrince <jacobszelko@gmail.com>
2023-10-14 22:45:56 -0400

    Commenting superpowers for all languages

---
+++ lua/plugins/nerdcommenter.lua
@@ -0,0 +1,14 @@
+return {
+	"preservim/nerdcommenter",
+	lazy = false,
+  keys = {
+      { "<Space><Space>", "<Plug>NERDCommenterSexy", mode = { "n", "v" }, { noremap = false } },
+      { "<Space>u", "<Plug>NERDCommenterToggle", mode = { "n", "v" }, { noremap = false } },
+    },
+  config = function()
+    vim.g.NERDCreateDefaultMappings = 0
+    vim.g.NERDSpaceDelims = 1
+    vim.g.NERDCommentEmptyLines = 1
+    vim.g.NERDTrimTrailingWhitespace = 1
+  end
+}
