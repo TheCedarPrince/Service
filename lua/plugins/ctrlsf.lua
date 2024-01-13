25b58c53c4b97381eab0cb188e040a640e255965
TheCedarPrince <jacobszelko@gmail.com>
2024-01-13 15:47:17 -0500

    Some small configuration tweaks

--- lua/plugins/ctrlsf.lua
+++ lua/plugins/ctrlsf.lua
@@ -2,16 +2,20 @@ return {
   "dyng/ctrlsf.vim",
   lazy = false,
   config = function()
-    vim.g.ctrlsf_ackprg = 'rg'  -- Use ag as the search program (requires ag to be installed)
+    vim.g.ctrlsf_ackprg = 'rg'  -- Use ripgrep as the search program (requires ripgrep to be installed)
     vim.g.ctrlsf_auto_close = 0  -- Automatically close the ctrlsf window when the search is complete
     vim.g.ctrlsf_auto_focus = 0  -- Automatically focus on the ctrlsf window when it opens
-    vim.g.ctrlsf_match_window_bottom = 1  -- Position the ctrlsf window at the bottom of the screen
+    vim.g.ctrlsf_match_window_bottom = 'bottom'  -- Position the ctrlsf window at the bottom of the screen
+    -- vim.g.ctrlsf_compact_position = 'bottom_inside' -- Where to place the compact window
+    vim.g.ctrlsf_position = 'bottom' -- Where to place the normal window
+    -- vim.g.ctrlsf_default_view_mode = 'compact' -- Default view mode of results window
     vim.g.ctrlsf_max_height = 10  -- Set the maximum height of the ctrlsf window to 10 lines
     vim.g.ctrlsf_auto_preview = 1
-    vim.g.ctrlsf_case_sensitive = 'no'
+    vim.g.ctrlsf_case_sensitive = 'yes'
     vim.g.ctrlsf_default_root = 'cwd' 
     vim.g.ctrlsf_fold_result = 0
     vim.g.ctrlsf_winsize = '50%'
+    vim.g.ctrlsf_backend = 'rg' -- Set backend to ripgrep
   end
 }
 
--- lua/plugins/undotree.lua
+++ lua/plugins/undotree.lua
@@ -2,6 +2,20 @@ return {
 	"mbbill/undotree",
 	lazy = false,
 	config = function()
-
+        vim.cmd([[
+        if has("persistent_undo")
+           let target_path = expand('~/.undodir')
+            " create the directory and any parent directories
+            " if the location does not exist.
+            if !isdirectory(target_path)
+                call mkdir(target_path, "p", 0700)
+            endif
+            let &undodir=target_path
+            set undofile
+        endif
+        ]])
+        vim.g.undotree_WindowLayout = 2
+        vim.g.undotree_SetFocusWhenToggle = 1
+        vim.g.undotree_ShortIndicators = 1
 	end
 }
