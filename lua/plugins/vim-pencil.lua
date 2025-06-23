a828385aec20a501bde53e96526ab0558f582e49
TheCedarPrince <jacobszelko@gmail.com>
2025-06-23 15:35:47 -0400

    Added initialization for default soft-pencil

--- lua/plugins/vim-pencil.lua
+++ lua/plugins/vim-pencil.lua
@@ -1,13 +1,7 @@
 return {
 	"preservim/vim-pencil",
 	lazy = false,
-	config = function()
-vim.g['pencil#cursorwrap'] = 0
-vim.g['pencil#conceallevel'] = 0
-vim.g['pencil#concealcursor'] = 'n'
-vim.g['pencil#wrapModeDefault'] = 'soft'
-vim.cmd [[
-        call pencil#init({'wrap': 'soft'})
-]]
-end 
+  init = function()
+    vim.g["pencil#wrapModeDefault"] = "soft"
+  end
 }
