0a7ef5f046428ce1a9e1c13c2cbaefe7338562cc
TheCedarPrince <jacobszelko@gmail.com>
2025-10-24 00:32:01 -0400

    Added colorscheme and highlighting tweak

---
+++ lua/plugins/binary.lua
@@ -0,0 +1,8 @@
+return {
+  "jackplus-xyz/binary.nvim",
+  opts = {
+    style = "dark",
+    use_default_reversed_group = true, -- Use the default `reversed_group` for a better startup experience
+    reversed_group = {TabLine = false, TabLineSel = false, TabLineFill = false}
+  },
+}
---
+++ lua/plugins/vim-cool.lua
@@ -0,0 +1,5 @@
+return {
+	"romainl/vim-cool",
+	lazy = false,
+  commit = "9ea940c"
+}
