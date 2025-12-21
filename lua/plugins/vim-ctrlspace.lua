5cc603f1155ebcc0e4a5ca5a9cbe74c253588084
TheCedarPrince <jacobszelko@gmail.com>
2025-12-20 22:15:32 -0500

    Add configuration for loading local cs_workspace file

--- lua/plugins/vim-ctrlspace.lua
+++ lua/plugins/vim-ctrlspace.lua
@@ -1,11 +1,15 @@
 return {
 	"vim-ctrlspace/vim-ctrlspace",
 	lazy = false,
-  commit = "f37c04c",
+  commit = "c8fdf9b",
   cond = function() return not vim.o.diff end,
   init = function()
       vim.g.CtrlSpaceSetDefaultMapping=true
       vim.g.CtrlSpaceUseTabline=true
       vim.g.CtrlSpaceDefaultMappingKey='<M-Space> '
+      --[[
+         [ vim.g.CtrlSpaceWorkspaceFile = '~/.config/nvim/cs_workspaces'
+         ]]
+      vim.g.CtrlSpaceProjectRootMarkers = ''
   end,
 }
