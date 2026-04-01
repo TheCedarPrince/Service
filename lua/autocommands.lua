b8be4124e957983c8cd5c2ccedd3de77ccd29f77
TheCedarPrince <jacobszelko@gmail.com>
2026-04-01 00:50:24 -0400

    Removed Goyo configuration

--- lua/autocommands.lua
+++ lua/autocommands.lua
@@ -16,37 +16,6 @@ vim.cmd [[
 	augroup end
 ]]
 
--- Description: Goyo mode support for making Goyo mode as minimalas possible. Also brings back incline support as Goyo disables it.
-vim.cmd [[
-function! s:goyo_enter()
-  " Hides mode from showing
-  set noshowmode 
-
-  " Hides the sign column
-  :set scl=no 
-
-  " ...
-endfunction
-function! s:goyo_leave()
-  " Makes the signcolumn match the background colorscheme
-  highlight clear SignColumn
-
-  hi GitSignsAdd guibg=NONE ctermfg=white
-  hi GitSignsChange guibg=NONE ctermfg=white
-  hi GitSignsDelete guibg=NONE ctermfg=white
-
-  " Brings mode back
-  set showmode 
-
-  " Recreate incline functionality
-  lua require'incline'.setup{}
-
-  " ...
-endfunction
-autocmd! User GoyoEnter nested call <SID>goyo_enter()
-autocmd! User GoyoLeave nested call <SID>goyo_leave()
-]]
-
 -- Description: Automatically update buffers if a change to the file system was detected
 vim.api.nvim_create_autocmd({ "BufEnter", "CursorHold", "CursorHoldI", "FocusGained" }, {
   command = "if mode() != 'c' | checktime | endif",
