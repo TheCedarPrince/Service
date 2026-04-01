897944590576178c416ea5cea8cb93c601ddc0c7
TheCedarPrince <jacobszelko@gmail.com>
2026-04-01 00:51:47 -0400

    Simplified goyo layout

--- lua/plugins/goyo.lua
+++ lua/plugins/goyo.lua
@@ -1,9 +1,27 @@
 return {
-	"junegunn/goyo.vim",
-	lazy = false,
+  "junegunn/goyo.vim",
+  lazy = false,
   commit = "fa0263d",
-	config = function()
-		vim.g.goyo_width = '85%' -- Default width
-		vim.g.goyo_height = '90%' -- Default height
-	end
+  config = function()
+    vim.g.goyo_width = "85%"
+    vim.g.goyo_height = "90%"
+
+    vim.cmd [[
+      function! s:goyo_enter()
+        set noshowmode
+        set scl=no
+      endfunction
+
+      function! s:goyo_leave()
+        set showmode
+        lua require('incline').setup{}
+      endfunction
+
+      augroup GoyoCallbacks
+        autocmd!
+        autocmd User GoyoEnter nested call <SID>goyo_enter()
+        autocmd User GoyoLeave nested call <SID>goyo_leave()
+      augroup END
+    ]]
+  end,
 }
