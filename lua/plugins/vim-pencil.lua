ae2f918b454419edca581866d20b4c0fa7f5d602
TheCedarPrince <jacobszelko@gmail.com>
2026-04-07 23:44:22 -0400

    Finally fixed auto soft pencil everywhere

--- lua/plugins/vim-pencil.lua
+++ lua/plugins/vim-pencil.lua
@@ -1,8 +1,20 @@
 return {
-	"preservim/vim-pencil",
-	lazy = false,
+  "preservim/vim-pencil",
+  lazy = false,
   version = "*",
   init = function()
     vim.g["pencil#wrapModeDefault"] = "soft"
-  end
+    vim.g["pencil#conceallevel"] = 2   -- tell pencil to use your desired conceallevel
+    vim.g["pencil#concealcursor"] = "nc" -- and concealcursor
+  end,
+  config = function()
+    local group = vim.api.nvim_create_augroup("Pencil", { clear = true })
+    vim.api.nvim_create_autocmd("BufEnter", {
+      group = group,
+      pattern = "*",
+      callback = function()
+        vim.fn["pencil#init"]()
+      end,
+    })
+  end,
 }
