f065f3b329cdc67f5e34cdd772af2b92d1154454
TheCedarPrince <jacobszelko@gmail.com>
2026-04-07 23:43:52 -0400

    Updated keybinds

--- lua/plugins/hop.lua
+++ lua/plugins/hop.lua
@@ -19,17 +19,6 @@ vim.api.nvim_set_keymap('n', '<C-h>l', "<cmd>lua require'hop'.hint_lines_skip_wh
 -- HopWord
 vim.api.nvim_set_keymap('n', '<C-h>w', "<cmd>lua require'hop'.hint_words()<cr>", {})
 
--- HopChar1
-vim.api.nvim_set_keymap('n', '<C-h>c', "<cmd>lua require'hop'.hint_char1()<cr>", {})
-
--- HopPattern
-vim.api.nvim_set_keymap('n', '<C-h>p', "<cmd>lua require'hop'.hint_patterns()<cr>", {})
-
-vim.api.nvim_command('highlight HopNextKey guifg=none guibg=none gui=none ctermfg=none cterm=bold')
-vim.api.nvim_command('highlight HopNextKey1 guifg=none guibg=none gui=none ctermfg=none cterm=bold')
-vim.api.nvim_command('highlight HopNextKey2 guifg=none guibg=none gui=none ctermfg=none')
-vim.api.nvim_command('highlight HopUnmatched guifg=none guibg=none guisp=none ctermfg=none')
-
     end
   }
 
