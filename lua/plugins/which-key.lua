beea3c86ab18c2832e49916ebcc2b213fed0ccc7
TheCedarPrince <jacobszelko@gmail.com>
2026-04-07 23:44:36 -0400

    Which key configuration and layouts

--- lua/plugins/which-key.lua
+++ lua/plugins/which-key.lua
@@ -50,11 +50,11 @@ return {
         scroll_up = "<c-u>", -- binding to scroll up inside the popup
       },
       window = {
-        border = "none", -- none, single, double, shadow
+        border = "rounded", -- none, single, double, shadow
         position = "bottom", -- bottom, top
         margin = { 1, 0, 1, 0 }, -- extra window margin [top, right, bottom, left]. When between 0 and 1, will be treated as a percentage of the screen size.
         padding = { 1, 2, 1, 2 }, -- extra window padding [top, right, bottom, left]
-        winblend = 0, -- value between 0-100 0 for fully opaque and 100 for fully transparent
+        winblend = 10, -- value between 0-100 0 for fully opaque and 100 for fully transparent
         zindex = 1000, -- positive value to position WhichKey above other floating windows.
       },
       layout = {
